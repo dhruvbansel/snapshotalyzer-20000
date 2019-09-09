64b4f4165637907dd3951a654ce7814e14225d14
dhruvbansel <banseldhruv@gmail.com>
2019-09-09 17:09:19 +0100

    Now we can take list all the snapshots as well as the most recent one as per our choice

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -16,6 +16,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshots(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state  == 'pending'
+
 @click.group()
 def cli():
     """Shotty manages snapshots"""
@@ -35,8 +39,10 @@ def instances():
 @snapshots.command('list')
 @click.option('--project',default=None,
     help ="only snapshots for project (tag Project:<name>)")
+@click.option('--all','list_all', default =False, is_flag=True,
+    help = "List all snapshots for each volume, not just the most recent one")
 
-def list_snapshots(project):
+def list_snapshots(project,list_all):
     "List EC2  snapshots of volumes"
     instances  = filter_instances(project)
     for i in instances:
@@ -51,6 +57,7 @@ def list_snapshots(project):
                 s.start_time.strftime("%c")
 
                 )))
+                if s.state =='completed' and not list_all: break
     return
 
 
@@ -89,6 +96,10 @@ def create_snapshots(project):
         i.stop()
         i.wait_until_stopped()
         for v in i.volumes.all():
+
+            if has_pending_snapshots(v):
+                print("skipping {0}, snapshot already in progress".format(v.id))
+                continue
             print("Creating snapshots of {0}".format(v.id))
             v.create_snapshot(Description = "Created by snapshotalyzer 20000")
 
